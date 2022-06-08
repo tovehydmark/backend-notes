437156ee22faa23175a21d605245aa7e788ec1fd
tovehydmark <tovehydmark@gmail.com>
2022-06-08 11:54:28 +0200

    update document in database works

--- routes/fetchDocuments.js
+++ routes/fetchDocuments.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const cors = require("cors");
 router.use(cors());
 
+// Get documents from database
+
 router.get("/", function (req, res) {
     req.app.locals.con.connect(function (err) {
         if (err) {
@@ -21,5 +23,29 @@ router.get("/", function (req, res) {
         });
     });
 });
+
+
+// Update document in database
+
+router.put("/", function (req, res) {
+
+    req.app.locals.con.connect(function (err) {
+
+        if (err) {
+            console.log(err);
+        }
+        let sql = `UPDATE notes SET documentText="${req.body.documentText}" WHERE documentId=${req.body.id}`
+
+        req.app.locals.con.query(sql, function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+
+            console.log(result);
+            res.json(result)
+        })
+    })
+})
+
 
 module.exports = router;
