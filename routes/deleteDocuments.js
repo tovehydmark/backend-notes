6ea76751e79369dbc3bdaef733275638d613c19f
tovehydmark <tovehydmark@gmail.com>
2022-06-10 10:32:50 +0200

    cleanup

--- routes/deleteDocuments.js
+++ routes/deleteDocuments.js
@@ -1,38 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-// const cors = require("cors");
-// router.use(cors());
+const cors = require("cors");
+router.use(cors());
 
-
-// Update document in database
-
-router.get("/", function (req, res) {
-    res.send("hej")
-})
-
+// Delete document in database
 router.delete("/", function (req, res) {
-
-    let documentId = (req.body.thisDocumentId)
+    let documentId = req.body.thisDocumentId;
 
     req.app.locals.con.connect(function (err) {
-
-
         if (err) {
             console.log(err);
         }
-        let sql = `DELETE FROM notes WHERE documentId=${Number(documentId)}`
+        let sql = `DELETE FROM notes WHERE documentId=${Number(documentId)}`;
 
         req.app.locals.con.query(sql, function (err, result) {
             if (err) {
                 console.log(err);
             }
 
-            res.json(result)
-        })
-    })
-})
-
-
+            res.json(result);
+        });
+    });
+});
 
 module.exports = router;
